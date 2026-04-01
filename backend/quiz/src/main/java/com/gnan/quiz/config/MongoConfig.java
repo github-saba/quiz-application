8decc441aa69cea77eb042f1570e0ed6822933df
github-saba <thiravidan@gmail.com>
2026-04-01 11:20:57 +0100

    Full-Stack Quiz Application (frontend + backend)

--- backend/quiz/src/main/java/com/gnan/quiz/config/MongoConfig.java
+++ backend/quiz/src/main/java/com/gnan/quiz/config/MongoConfig.java
@@ -1,4 +1,22 @@
 package com.gnan.quiz.config;
 
-public class MongoConfig {
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoClientConfiguration;
+
+@Configuration
+public class MongoConfig extends AbstractMongoClientConfiguration {
+
+    @Override
+    protected String getDatabaseName() {
+        return "quizdb";
+    }
+
+    @Bean
+    @Override
+    public MongoClient mongoClient() {
+        return MongoClients.create("MONGO_URI");
+    }
 }
